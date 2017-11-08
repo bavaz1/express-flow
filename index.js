e2e36f36aa77f06386a10d1215791ae544234367
bavaz1 <kismadar12@gmail.com>
2017-11-08 10:20:33 +0100

    nov8. before the database coming

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const path = require('path');
 
 let students = require('./data');
 
 const app = express();
+const hbs = require('hbs');
 
 app.set('views', './views/');
 app.set('view engine', 'hbs');
@@ -12,12 +15,42 @@ app.engine('hbs', require('hbs').__express);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(methodOverride('_method'));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
 app.get('/students', function (request, response) {
   const page = parseInt(request.query.page) || 1;
   const limit = 5;
   let skip = (page - 1) * limit;
   let next = page + 1;
   let previous = page - 1;
+  let possibleNext;
+  let possiblePrev;
+  let pages = [];
+
+  for (let i = 0; i < students.length / limit; i++) {
+    pages.push(i + 1);
+  }
+
+  if (students.length / 5 > page) {
+    possibleNext = true;
+  } else {
+    possibleNext = false;
+  }
+
+  if (page > 1) {
+    possiblePrev = true;
+  } else {
+    possiblePrev = false;
+  }
+
+  hbs.registerHelper('currentPage', (pages, page, options) => {
+    if (pages === page) {
+      return options.fn(page);
+    }
+
+    return options.inverse(this);
+  });
 
   response.format({
     'text/html': function () {
@@ -25,15 +58,25 @@ app.get('/students', function (request, response) {
         students: students.slice(skip, limit + skip),
         next: next,
         previous: previous,
-        page: page
+        page: page,
+        possibleNext: possibleNext,
+        possiblePrev: possiblePrev,
+        pages: pages
       });
     },
     'application/json': function () {
       response.json({students: students});
     }
   });
+});
 
-  //response.json({students: students.slice(skip, limit + skip)});
+app.get('/students/create', function (request, response) {
+  response.render('students/create');
+});
+
+app.get('/students/:name/update', function (request, response) {
+  const student = students.find(student => student.name === request.params.name);
+  response.render('students/update', {student: student});
 });
 
 app.get('/students/:name', function (request, response) {
@@ -47,8 +90,6 @@ app.get('/students/:name', function (request, response) {
       response.json({student: student});
     }
   });
-
-  //response.json({student: student});
 });
 
 app.post('/students', function (request, response) {
@@ -61,18 +102,30 @@ app.post('/students', function (request, response) {
 
   students.push(newStudent);
 
-  response.json({student: newStudent});
+  response.format({
+    'text/html': function () {
+      response.redirect('/students');
+    },
+    'application/json': function () {
+      response.json({student: newStudent});
+    }
+  });
 });
 
 app.delete('/students/:name', function (request, response) {
   students = students.filter(student => student.name !== request.params.name);
 
-  response.json({students: students});
+  response.format({
+    'text/html': function () {
+      response.redirect('/students/');
+    },
+    'application/json': function () {
+      response.json({students: students});
+    }
+  });
 });
 
 app.put('/students/:name', function (request, response) {
-  const student = students.find(student => student.name === request.params.name);
-  let index = students.indexOf(student);
   const changedStudent = {
     name: request.body.name,
     sex: request.body.sex,
@@ -80,9 +133,18 @@ app.put('/students/:name', function (request, response) {
     age: request.body.age
   };
 
-  students[index] = changedStudent;
+  let indexOfUpdateStudent = students.indexOf(students.find(student => student.name === request.params.name));
+  students.splice(indexOfUpdateStudent, 1);
+  students.splice(indexOfUpdateStudent, 0, changedStudent);
 
-  response.json({students: changedStudent});
+  response.format({
+    'text/html': function () {
+      response.redirect('/students/' + changedStudent.name);
+    },
+    'application/json': function () {
+      response.json({students: changedStudent});
+    }
+  });
 });
 
 app.get('/scores', function (request, response) {
